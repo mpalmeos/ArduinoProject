403b9d01782bc77cdeb679d0c92a5d72bb77bb6d
mpalmeos <maie.palmeos@itcollege.ee>
2017-10-29 10:17:09 +0200

    Add lab2 code

--- src/main.c
+++ src/main.c
@@ -1,16 +1,24 @@
 #include <avr/io.h>
 #include <util/delay.h>
+#define BLINK_DELAY_MS1 500
+#define BLINK_DELAY_MS2 500
+#define BLINK_DELAY_MS3 500
 
-#define BLINK_DELAY_MS 1000
+void main (void){
+    
+    DDRA=0x15;
 
-void main (void) 
-{
-	DDRB |= _BV(DDB7);
-	
-	while(1) {
-		PORTB |= _BV(PORTB7);
-		_delay_ms(BLINK_DELAY_MS);
-		PORTB &= ~_BV(PORTB7);
-		_delay_ms(BLINK_DELAY_MS);
-	}
+    while(1){
+        PORTA |= _BV(PORTA0);
+        _delay_ms(BLINK_DELAY_MS1);
+        PORTA &= ~ _BV(PORTA0);
+
+        PORTA |= _BV(PORTA2);
+        _delay_ms(BLINK_DELAY_MS2);
+        PORTA &= ~ _BV(PORTA2);
+
+        PORTA |= _BV(PORTA4);
+        _delay_ms(BLINK_DELAY_MS3);
+        PORTA &= ~ _BV(PORTA4);
+    }
 }
