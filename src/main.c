d435673bdae0bd8f532395c5ea9f9664f9c89988
mpalmeos <maie.palmeos@itcollege.ee>
2017-11-09 21:34:10 +0200

    Fixed mistakes mentioned in lab2 feedback: code format and LED light

--- src/main.c
+++ src/main.c
@@ -6,19 +6,23 @@
 
 void main (void){
     
-    DDRA=0x15;
+DDRA=0x15;
+DDRB |= _BV(DDB7);
 
-    while(1){
-        PORTA |= _BV(PORTA0);
-        _delay_ms(BLINK_DELAY_MS1);
-        PORTA &= ~ _BV(PORTA0);
-
-        PORTA |= _BV(PORTA2);
-        _delay_ms(BLINK_DELAY_MS2);
-        PORTA &= ~ _BV(PORTA2);
-
-        PORTA |= _BV(PORTA4);
-        _delay_ms(BLINK_DELAY_MS3);
-        PORTA &= ~ _BV(PORTA4);
+while(1){
+    
+    PORTB &= ~_BV(PORTB7);
+    
+    PORTA |= _BV(PORTA0);
+    _delay_ms(BLINK_DELAY_MS1);
+    PORTA &= ~ _BV(PORTA0);
+    
+    PORTA |= _BV(PORTA2);
+    _delay_ms(BLINK_DELAY_MS2);
+    PORTA &= ~ _BV(PORTA2);
+    
+    PORTA |= _BV(PORTA4);
+    _delay_ms(BLINK_DELAY_MS3);
+    PORTA &= ~ _BV(PORTA4);
     }
 }
