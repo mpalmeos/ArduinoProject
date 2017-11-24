19a41cc39a4a80d7a9c2bb48a0bcd29a041ee067
Maie Palmeos <maie.palmeos@itcollege.ee>
2017-11-24 11:13:36 +0200

    Fixes concerninf lab3 feedback

--- src/main.c
+++ src/main.c
@@ -1,16 +1,16 @@
+#include <stdio.h>
+#define __ASSERT_USE_STDERR
+#include <assert.h>
 #include <avr/io.h>
 #include <util/delay.h>
-#include <stdio.h>
-#include <assert.h>
 #include "uart.h"
-#define _ASSERT_USE_STDERR
 #define BLINK_DELAY_MS 100
 
 
 static inline void init_leds(void)
 {
     //Set port A pins 22, 24 and 26 as output.
-    DDRA=0x15;
+    DDRA = 0x15;
     //Set port B pin 7 as output (onboard yellow LED)
     DDRB |= _BV(DDB7);
 }
@@ -32,20 +32,17 @@ static inline void blink_leds(void)
 {
     //Set port B pin 7 output to low to turn onboard LED off
     PORTB &= ~_BV(PORTB7);
-
-    /*Set port A pin 22 to high to turn red LED on, wait and 
-    then turn it to low to turn it off*/ 
+    /*Set port A pin 22 to high to turn red LED on, wait and
+    then turn it to low to turn it off*/
     PORTA |= _BV(PORTA0);
     _delay_ms(BLINK_DELAY_MS);
     PORTA &= ~ _BV(PORTA0);
-
-     /*Set port A pin 22 to high to turn green LED on, wait and 
+    /*Set port A pin 22 to high to turn green LED on, wait and
     then turn it to low to turn it off*/
     PORTA |= _BV(PORTA2);
     _delay_ms(BLINK_DELAY_MS);
     PORTA &= ~ _BV(PORTA2);
-
-     /*Set port A pin 22 to high to turn blue LED on, wait and 
+    /*Set port A pin 22 to high to turn blue LED on, wait and
     then turn it to low to turn it off*/
     PORTA |= _BV(PORTA4);
     _delay_ms(BLINK_DELAY_MS);
@@ -58,29 +55,7 @@ void main (void)
     init_leds();
     init_errcon();
 
-    //Test assert - REMOVE IN FUTURE LABS
-    char *array;
-    uint32_t i = 1;
-    extern int __heap_start, *__brkval;
-    int v;
-    array = malloc( i * sizeof(char));
-    assert(array);
-    //End test assert
-
-    while(1)
-    {
+    while (1) {
         blink_leds();
-        
-        //Test assert - REMOVE IN FUTURE LABS
-        /*
-         * Increase memory allocated for array by 100 chars
-         * until we have eaten it all and print space between stack and heap.
-         * That is how assert works in run-time.
-         */
-        array = realloc( array, (i++ * 100) * sizeof(char));
-        fprintf(stderr, "%d\n",
-                (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval));
-        assert(array);
-        //End test assert
     }
 }
--- src/uart.c
+++ src/uart.c
@@ -13,7 +13,7 @@
 /* http://www.cs.mun.ca/~rod/Winter2007/4723/notes/serial/serial.html */
 
 
-void simple_uart0_init(void) 
+void simple_uart0_init(void)
 {
     UBRR0H = UBRRH_VALUE;
     UBRR0L = UBRRL_VALUE;
@@ -22,24 +22,26 @@ void simple_uart0_init(void)
 #else
     UCSR0A &= ~(_BV(U2X0));
 #endif
-    UCSR0C = _BV(UCSZ01) | _BV(UCSZ00); /* 8-bit data */ 
-    UCSR0B = _BV(RXEN0) | _BV(TXEN0);   /* Enable RX and TX */    
+    UCSR0C = _BV(UCSZ01) | _BV(UCSZ00); /* 8-bit data */
+    UCSR0B = _BV(RXEN0) | _BV(TXEN0);   /* Enable RX and TX */
 }
 
 
-int simple_uart0_putchar(char c, FILE *stream) 
+int simple_uart0_putchar(char c, FILE *stream)
 {
     (void) stream;
+
     if (c == '\n') {
         simple_uart0_putchar('\r', stream);
     }
+
     loop_until_bit_is_set(UCSR0A, UDRE0);
     UDR0 = c;
     return 0;
 }
 
 
-int simple_uart0_getchar(FILE *stream) 
+int simple_uart0_getchar(FILE *stream)
 {
     (void) stream;
     loop_until_bit_is_set(UCSR0A, RXC0);
@@ -51,7 +53,6 @@ void simple_uart1_init(void)
 {
     UBRR1H = UBRRH_VALUE;
     UBRR1L = UBRRL_VALUE;
-    
 #if USE_2X
     UCSR1A |= _BV(U2X1);
 #else
@@ -65,9 +66,11 @@ void simple_uart1_init(void)
 int simple_uart1_putchar(char c, FILE *stream)
 {
     (void) stream;
+
     if (c == '\n') {
         simple_uart1_putchar('\r', stream);
     }
+
     loop_until_bit_is_set(UCSR1A, UDRE1);
     UDR1 = c;
     return 0;
